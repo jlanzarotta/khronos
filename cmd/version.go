997f610fe164f2583e53563ccda641028963c84b
Jeff Lanzarotta <jlanzarotta@matw.com>
2024-09-18 08:08:39 -0400

    Update version command to how display the full BSD 3-Clause license.

--- cmd/version.go
+++ cmd/version.go
@@ -1,9 +1,11 @@
 package cmd
 
 import (
+	"khronos/constants"
 	"log"
 	"time"
 
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
@@ -25,7 +27,38 @@ func init() {
 }
 
 func runVersion(_ *cobra.Command, _ []string) {
-	log.Printf("  Version: " + BuildVersion + "\n" +
-		"Copyright: (c) 2018-" + time.Now().Format("2006") +
-		" Jeff Lanzarotta, All rights reserved\n  Born on: " + BuildDateTime + "\n")
+	log.Printf("%s %s\nBorn on: %s\n" +
+"------------------------------------------------------------------------------\n" +
+"BSD 3-Clause License\n\n" +
+"Copyright (c) 2018-%s, Jeff Lanzarotta All rights reserved.\n" +
+"\n" +
+"Redistribution and use in source and binary forms, with or without\n" +
+"modification, are permitted provided that the following conditions are met:\n" +
+"\n" +
+"1. Redistributions of source code must retain the above copyright notice, this\n" +
+"   list of conditions and the following disclaimer.\n" +
+"\n" +
+"2. Redistributions in binary form must reproduce the above copyright notice,\n" +
+"   this list of conditions and the following disclaimer in the documentation\n" +
+"   and/or other materials provided with the distribution.\n" +
+"\n" +
+"3. Neither the name of the copyright holder nor the names of its\n" +
+"   contributors may be used to endorse or promote products derived from\n" +
+"   this software without specific prior written permission.\n" +
+"\n" +
+"THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"\n" +
+"AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n" +
+"IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE\n" +
+"DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE\n" +
+"FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL\n" +
+"DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR\n" +
+"SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER\n" +
+"CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,\n" +
+"OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE\n" +
+"OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n" +
+"------------------------------------------------------------------------------\n", 
+	color.YellowString(constants.APPLICATION_NAME), BuildVersion, BuildDateTime, time.Now().Format("2006"))
+	
+//	Copyright: (c) 2018-%s Jeff Lanzarotta, All rights reserved\n  Born on: %s\n",
+//		color.YellowString(constants.APPLICATION_NAME), BuildVersion, time.Now().Format("2006"), BuildDateTime)
 }
