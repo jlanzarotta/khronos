0c2f4241e49c48dc8e773066ebacacbb7ef0cca8
Jeff Lanzarotta <jlanzarotta@matw.com>
2024-12-12 16:10:30 -0500

    style: typo
    Fixed typo.

--- cmd/nuke.go
+++ cmd/nuke.go
@@ -20,7 +20,7 @@ import (
 var nukeCmd = &cobra.Command{
 	Use:   "nuke",
 	Short: "Nukes entries from the sqlite database",
-	Long:  `As you continuously add completed entries, the database continues to go unbounded.  The nuke command allows you to manage the database size.`,
+	Long:  `As you continuously add completed entries, the database continues to grow unbounded.  The nuke command allows you to manage the size of your database.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		runNuke(cmd, args)
 	},
