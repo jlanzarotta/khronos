8629e87ee70b348992f7628549b26b4717f8c9f5
jlanzarotta <Jeff.Lanzarotta@pyramidsoftware.com>
2026-08-05 14:14:25 -0400

    fix: working on report formatting

--- cmd/report.go
+++ cmd/report.go
@@ -349,12 +349,12 @@ func reportByEntry(entries []models.Entry) {
 			},
 			{
 				Number:   4,
-				WidthMin: 24,
+				WidthMin: 10,
 				WidthMax: 24,
 			},
 			{
 				Number:   5,
-				WidthMin: 41,
+				WidthMin: 10,
 				WidthMax: 41,
 			},
 			{
@@ -393,7 +393,7 @@ func reportByEntry(entries []models.Entry) {
 			},
 			{
 				Number:   6,
-				WidthMin: 25,
+				WidthMin: 10,
 				WidthMax: 25,
 			},
 			{
